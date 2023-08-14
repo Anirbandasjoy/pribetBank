84b9b3d8a90f87ed729d23dd2f8806c54c09b802
joydas <joy600508@gmail.com>
2023-08-15 04:32:43 +0600

    added

--- js/diposite.js
+++ js/diposite.js
@@ -2,10 +2,6 @@ document.getElementById("depositeBtn").addEventListener("click", () => {
   const depsiteAmount = Number(document.getElementById("inputDeposite").value);
   const prvDeposite = document.getElementById("dipositeAmount");
   const currentDeposite = Number(prvDeposite.innerText);
-  if (typeof prvDeposite.innerText === "string") {
-    alert("Please Provide Number");
-    return;
-  }
 
   const totalDeposite = currentDeposite + depsiteAmount;
   prvDeposite.innerText = totalDeposite;
